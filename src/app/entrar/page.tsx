50a2fc51e6e321cfd37fca1d157ba6487af4c957
gabehercules <35200922+gabehercules@users.noreply.github.com>
2023-06-29 23:36:40 -0300

    update 1.0.3 created login page

--- src/app/entrar/page.tsx
+++ src/app/entrar/page.tsx
@@ -1,14 +1,49 @@
 'use client';
 
+import Logo from '../../components/Logo';
+
 export default function Page() {
   return (
-    <div className="">
-      <div>
-        <form action="">
-          <input type="text" id="username" name="username" />
-          <input type="password" id="password" name="password" />
+    <div className="flex flex-col h-[80vh] items-center justify-center">
+      <>
+        <Logo />
+        <form action="" className="w-[400px] flex flex-col gap-4 p-6 rounded">
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Usuário master"
+            className="bg-[#101010] border rounded-md border-[#303030] placeholder:text-white/30 placeholder:text-sm"
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Senha super secreta"
+            className="bg-[#101010] border rounded-md border-[#303030] placeholder:text-white/30 placeholder:text-sm"
+          />
+          <button
+            type="submit"
+            className="bg-brand-green text-[#101010] p-2 rounded-md"
+          >
+            Cola meu patrão
+          </button>
+          <p className="text-sm text-white/40">
+            Essa é uma área interna privada. Mas se ficou curioso para ver como
+            é por dentro, pode conferir no meu protótipo do Figma, ficou
+            igualzinho!{' '}
+            <a href="#" className="underline text-white/60">
+              Ver protótipo
+            </a>
+          </p>
+          <p className="text-sm text-white/40">
+            Se você é dev, pode clonar o repositório, ele é open source ;){' '}
+            <a href="#" className="underline text-white/60">
+              Ver repo
+            </a>
+          </p>
         </form>
-      </div>
+      </>
     </div>
   );
 }
